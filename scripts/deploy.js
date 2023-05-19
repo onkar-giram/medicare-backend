37526cdbdf9c4c9bff8f440d9c31a54dde6da726
Onkar Giram <meonkargiram@gmail.com>
2023-05-19 09:43:03 +0530

    final commit

---
+++ scripts/deploy.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+	const Contract = await hre.ethers.getContractFactory("Contract");
+	const contract = await Contract.deploy();
+
+	await contract.deployed();
+
+	console.log("Contract deployed to:", contract.address);
+}
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
